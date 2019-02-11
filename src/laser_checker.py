0eb2c34a6142d1e371b80cf17a0c3cc5975103a1
jgomezRobotnik <jgomez@robotnik.es>
2019-02-11 14:32:50 +0100

    Version 1.0. Log is not working

--- src/laser_checker.py
+++ src/laser_checker.py
@@ -2,110 +2,53 @@
 # coding=utf-8
 
 import rospy
-import evdev
-import datetime
-from evdev import InputDevice, categorize, ecodes
-from std_msgs.msg import Header
+import time
+from sensor_msgs.msg import LaserScan
 
-'''
-  See where is the device "BarCode WPM ..."
-  $ lsusb
-  - Find BUS 00x Device 0xx: ID idVendor:idProduct Winbond Electronics Corp.
-  - Create an udev rule
-  $ sudo nano /etc/udev/rules.d/40-scanner.rules
-  - And write (modifying idVendor and idProduct if is necessary):
-  KERNEL=="event[0-9]*", SUBSYSTEM=="input", ATTRS{idVendor}=="0416", ATTRS{idProduct}=="c141", SYMLINK="input/barcode_scanner",GROUP="dialout", MODE="0666"
-  - Execute:
-  $ sudo service udev restart
-  $ sudo udevadm trigger
-  - Check if the link has been created:
-  $ ls -ln /dev/input | grep barcode
+DEFAULT_FREQ = 40.0 # Hz, 0.025 s
 
-'''
+rospy.init_node('laser_checker')
 
-THIS NODE IS A COPY OF THE barcode_scanner NODE AS A KIND OF TEMPLATE.
+tini = time.time()
+tlast = time.time()
 
-keys = {
-    # Scancode: ASCIICode
-    0: None, 1: u'ESC', 2: u'1', 3: u'2', 4: u'3', 5: u'4', 6: u'5', 7: u'6', 8: u'7', 9: u'8',
-    10: u'9', 11: u'0', 12: u'-', 13: u'=', 14: u'BKSP', 15: u'TAB', 16: u'Q', 17: u'W', 18: u'E', 19: u'R',
-    20: u'T', 21: u'Y', 22: u'U', 23: u'I', 24: u'O', 25: u'P', 26: u'[', 27: u']', 28: u'CRLF', 29: u'LCTRL',
-    30: u'A', 31: u'S', 32: u'D', 33: u'F', 34: u'G', 35: u'H', 36: u'J', 37: u'K', 38: u'L', 39: u';',
-    40: u'"', 41: u'`', 42: u'LSHFT', 43: u'\\', 44: u'Z', 45: u'X', 46: u'C', 47: u'V', 48: u'B', 49: u'N',
-    50: u'M', 51: u',', 52: u'.', 53: u'/', 54: u'RSHFT', 56: u'LALT', 100: u'RALT'
-}
+noKeepRateMessage = [] # (time stamp, actual freq)
+emptyMessage = [] # time stamp
 
-class BarCodeScanner:
+# Create subscriber callback
+def callback(msg):
 
-  def __init__(self, args):
-    self.topic = args['topic']
-    self.port = args['port']
-    self.dev = InputDevice(self.port)
-    self.seq = 0
-    self.init_time = datetime.datetime.now()
-    rospy.loginfo(self.dev)
+  global tini, tlast, log, noKeepRateMessage, emptyMessage
 
-    # Create publisher
-    self.pub = rospy.Publisher(self.topic, Header, queue_size=10)
+  # Time since last message
+  tact = time.time()
+  tfreq = tact - tlast
+  tactfilt = tact - tini
+  tlast = time.time()
 
-  def scanBarcode(self):
-    barcode = ''
-    while True:
-        event = self.dev.read_one()
-        if event is None and barcode == '':
-          #There are blank events in between characters, 
-          #so we don't want to break if we've started
-          #reading them
-          if rospy.is_shutdown():
-					  exit()
-          break #nothing of importance, start a new read. 
-        try:
-          if event is not None:
-            if event.type == ecodes.EV_KEY:
-              data = categorize(event)
-              if data.keystate == 0 and data.scancode != 42: # Catch only keyup, and not Enter
-                if data.scancode == 28: #looking return key to be pressed
-                  #rospy.loginfo(barcode)
-                  msg = Header()
-                  msg.seq = self.seq
-                  self.seq = self.seq + 1
-                  time_diff = datetime.datetime.now() - self.init_time
-                  msg.stamp.secs = time_diff.seconds
-                  msg.stamp.nsecs = time_diff.microseconds * 1000
-                  msg.frame_id = barcode
-                  self.pub.publish(msg)
-                  barcode = ''
-                else:
-                    barcode += keys[data.scancode] # add the new char to the barcode
-        except AttributeError:
-            rospy.logerr("error parsing stream")
-            return 'SOMETHING WENT WRONG'
+  # If node is initialised
+  if (tactfilt > 2):
+
+    # Save the values in a txt
+    log = open("laser_checker.log", "a") # Append the new values
+
+    # If frequency is too slow
+    if tfreq > 1.5*(1 / DEFAULT_FREQ): # In seconds
+      noKeepRateMessage.append((tactfilt, 1/tfreq))
+      log.write(str(tactfilt)+":  "+str(1/tfreq)+" Hz (too slow frequency)\n")
+
+    # If empty message
+    if len(msg.ranges) == 0:
+      emptyMessage.append(tactfilt)
+      log.write(str(tactfilt)+":  EMPTY MESSAGE ERROR\n")
+
+    log.close()
+
+  print(noKeepRateMessage)
+  print("-------")
+  print(emptyMessage)
+  print("***********************")
 
 
-if __name__ == "__main__":
-  
-  rospy.init_node("barcode_scanner")
-
-  _name = rospy.get_name().replace('/','')
-
-  arg_defaults = {
-    'topic': 'barcode_scanner',
-    'port': '/dev/input/barcode_scanner'
-  }
-  
-  args = {}
-  
-  for name in arg_defaults:
-    try:
-      if rospy.search_param(name): 
-        args[name] = rospy.get_param('~%s'%(name)) # Adding the name of the node, because the param has the namespace of the node
-      else:
-        args[name] = arg_defaults[name]
-    except rospy.ROSException, e:
-      rospy.logerr('%s: %s'%(e, _name))
-  
-
-  rc_node = BarCodeScanner(args)
-
-  while(True):
-    rc_node.scanBarcode()
+sub = rospy.Subscriber('/rb1_base/front_laser/scan', LaserScan, callback)
+rospy.spin()
